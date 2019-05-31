60144d25849dd0719b8390fd751d512fb3a8b820
Adam Copley <adam.copley@hmrc.gsi.gov.uk>
2019-05-31 15:33:55 +0100

    Add log dir cleanup

--- installation_files/install.sh
+++ installation_files/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Clear log folder if already existing
+LOG_DIR="/opt/jobscheduler/data/scheduler/logs"
+if [ -d "${LOG_DIR}" ] ; then
+  rm -rf ${LOG_DIR}
+fi
+
 # Prepare jobscheduler_install.xml
 ./prep-config.sh
 
